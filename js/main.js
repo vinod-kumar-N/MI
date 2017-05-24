e6728ffb7f9218d3aefa6a5f89a9a97294b189f6
vinod-kumar-n <vinodn.9635@gmail.com>
2017-05-24 21:39:30 +0530

    adding all

--- js/main.js
+++ js/main.js
@@ -3,9 +3,61 @@ $(document).ready(function(){
     $('#createXML').on('click', function(e){
         e.preventDefault();
         var getRecordNo = $('#recordNo').val();
-        var xmlVersion = '<?xml version="1.0" encoding="UTF-8"?>';
-        var RCNo = '<recordNo>'+ getRecordNo+'</recordNo>'
-        var textFile = new Blob([xmlVersion,RCNo], {
+        var CustomerName = $('#custName').val();
+        var EmailAddress = $('#email').val();
+        var resAdd = $('#resAdd').val();
+        var City_1 = $('#City_1').val();
+        var State_1 = $('#State_1').val();
+        var Zip_1 = $('#Zip_1').val();
+        var PhNo_1 = $('#PhNo_1').val();
+        var Country_1 = $('#Country_1').val();
+        var Sex_1 = $('#Sex_1').val();
+        var D_Birth = $('#D_Birth').val();
+        var height = $('#Height').val();
+        var weight = $('#Weight').val();
+        var BldG = $('#BG').val();
+        var BillName = $('#BillName').val();
+        var ShipName = $('#ShipName').val();
+        var City_2 = $('#City_2').val();
+        var State_2 = $('#State_2').val();
+        var Zip_2 = $('#Zip_2').val();
+        var Country_2 = $('#Country_2').val();
+        var PhNo_2 = $('#PhNo_2').val();
+        var Alcohlic = $('#Alcohlic').val();
+        var Smoker = $('#Smoker').val();
+        var PastS = $('#PS').val();
+        var Diabetic = $('#Diabetic').val();
+        var Allergiesd = $('#Allergiesd').val();
+        var policyNo = $('#policyNo').val();
+        var D_B_Life = $('#D_B_Life').val();
+        var P_Inst = $('#P_Inst').val();
+        var Name_P = $('#Name_P').val();
+        var STMa = $('#STM').val();
+        var STM_Code = $('#STM_Code').val();
+        var DOBa = $('#DOB').val();
+        var Sex_2 = $('#Sex_2').val();
+        var CNa = $('#CN').val();
+        var Medicine = $('#Medicine').val();
+        var Dosage = $('#Dosage').val();
+        var Tablets = $('#Tablets').val();
+        var pillRate = $('#pillRate').val();
+        var ShipCost = $('#ShipCost').val();
+        var Cost = $('#Cost').val();
+        var TAa = $('#TA').val();
+        var remarks = $('#remarks').val();
+        var date =  new Date().toString();
+        var RCNo = '<DataM>\n<ImageName></ImageName>\n'+'<RecordNo>'+ getRecordNo+'</RecordNo>\n<CustomerName>'+CustomerName+'</CustomerName>\n<EmailAddress>'+EmailAddress+
+        '</EmailAddress>\n<ResAddress>'+resAdd+'</ResAddress>\n<City_1>'+City_1+'</City_1>\n<State_1>'+State_1+'</State_1>\n<Zip_1>'+Zip_1+'</Zip_1>\n<PhNo_1>'+PhNo_1+
+        '</PhNo_1>\n<Country_1>'+Country_1+'</Country_1>\n<Sex_1>'+Sex_1+'</Sex_1>\n<D_Birth>'+D_Birth+'</D_Birth>\n<Height>'+height+'</Height>\n<Weight>'+weight+
+        '</Weight>\n<Blood_Group>'+BldG+'</Blood_Group>\n<BillingName>'+BillName+'</BillingName>\n<ShipperName>'+ShipName+'</ShipperName>\n<City_2>'+City_2+
+        '</City_2>\n<State_2>'+State_2+'</State_2>\n<Zip_2>'+Zip_2+'</Zip_2>\n<Country_2>'+Country_2+'</Country_2>\n<PhNo_2>'+PhNo_2+'</PhNo_2>\n<Alcoholic>'+Alcohlic+
+        '</Alcoholic>\n<Smoker>'+Smoker+'</Smoker>\n<PastSug>'+PastS+'</PastSug>\n<Diabetic>'+Diabetic+'</Diabetic>\n<Allergiesd>'+Allergiesd+
+        '</Allergiesd>\n<PloicyNo>'+policyNo+'</PloicyNo>\n<D_B_Life_Assure>'+D_B_Life+'</D_B_Life_Assure>\n<P_Inst>'+P_Inst+'</P_Inst>\n<Name_P_Holder>'+Name_P+
+        '</Name_P_Holder>\n<STM_Name>'+STMa+'</STM_Name>\n<STM_Code>'+STM_Code+'</STM_Code>\n<DOB>'+DOBa+'</DOB>\n<Sex_2>'+Sex_2+'</Sex_2>\n<CardName>'+CNa+
+        '</CardName>\n<Medicine>'+Medicine+'</Medicine>\n<Dosage>'+Dosage+'</Dosage>\n<Tablets>'+Tablets+'</Tablets>\n<PillRate>$'+pillRate+
+        '</PillRate>\n<Cost>$'+Cost+'</Cost>\n<ShippingCost>$'+ShipCost+'</ShippingCost>\n<TotalAmount>$'+TAa+'</TotalAmount>\n<Remarks>'+remarks+
+        '</Remarks><UserID>ethos_user5</UserID><CreateDate>'+date+'</CreateDate><UpdateDate></UpdateDate> </DataM>'
+        var textFile = new Blob([RCNo], {
             type: 'text/plain'
         });
         invokeSaveAsDialog(textFile, getRecordNo+'.xml');
@@ -72,7 +124,7 @@ preFill = function(a){
         $('#DOB').val($(that).val());
         $('#D_B_Life').val($(that).val());
     }
-    
+
 }
 
 
