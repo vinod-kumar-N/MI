f035718083d4625e7d323c0526d1d8b6be674369
Vinodkumar N <24507@ITCINFOTECH.COM>
2017-05-24 11:47:20 +0530

    Adding xml Generation

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    $('#createXML').on('click', function(e){
+        e.preventDefault();
+        var getRecordNo = $('#recordNo').val();
+        var xmlVersion = '<?xml version="1.0" encoding="UTF-8"?>';
+        var textFile = new Blob([xmlVersion,getRecordNo], {
+            type: 'text/plain'
+        });
+        invokeSaveAsDialog(textFile, getRecordNo+'.xml');
+    })
+    function invokeSaveAsDialog(file, fileName) {
+    if (!file) {
+        throw 'Blob object is required.';
+    }
+
+    if (!file.type) {
+        file.type = 'video/webm';
+    }
+
+    var fileExtension = file.type.split('/')[1];
+
+    if (fileName && fileName.indexOf('.') !== -1) {
+        var splitted = fileName.split('.');
+        fileName = splitted[0];
+        fileExtension = splitted[1];
+    }
+
+    var fileFullName = (fileName || (Math.round(Math.random() * 9999999999) + 888888888)) + '.' + fileExtension;
+
+    if (typeof navigator.msSaveOrOpenBlob !== 'undefined') {
+        return navigator.msSaveOrOpenBlob(file, fileFullName);
+    } else if (typeof navigator.msSaveBlob !== 'undefined') {
+        return navigator.msSaveBlob(file, fileFullName);
+    }
+
+    var hyperlink = document.createElement('a');
+    hyperlink.href = URL.createObjectURL(file);
+    hyperlink.target = '_blank';
+    hyperlink.download = fileFullName;
+
+    if (!!navigator.mozGetUserMedia) {
+        hyperlink.onclick = function() {
+            (document.body || document.documentElement).removeChild(hyperlink);
+        };
+        (document.body || document.documentElement).appendChild(hyperlink);
+    }
+
+    var evt = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+
+    hyperlink.dispatchEvent(evt);
+
+    if (!navigator.mozGetUserMedia) {
+        URL.revokeObjectURL(hyperlink.href);
+    }
+}
+preFill = function(a){
+    var that = a;
+    $('#BillName').val($(that).val())
+}
+});
